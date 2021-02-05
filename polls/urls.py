e73bc857a7f5111e165462d169bf241b26e0a581
Ny-Tran <tntran1@ualberta.ca>
2021-02-04 18:46:16 -0700

    finish lab 4

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+# To call the view, we need to map it to a URL - 
+# and for this we need a URLconf in the polls directory 
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
